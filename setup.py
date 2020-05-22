255a2b9c14b7e81b8b99cfd83fa8fcb86346ae1a
Martin Roa Villescas <m.roa.villescas@tue.nl>
2020-05-23 00:58:58 +0200

    Remove HISTORY.rst

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = ['Click>=7.0', 'evdev', 'appdirs', 'elevate']
 
 setup_requirements = [ ]
@@ -39,7 +36,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='onthefly',
     name='onthefly',
