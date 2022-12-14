a310454f19d135554dcdaf405438e7ca71dc9f31
Martin Roa Villescas <mroavi@gmail.com>
2022-12-14 11:47:53 +0100

    Bump version to 1.1.5

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mroavi/onthefly',
-    version='1.1.4',
+    version='1.1.5',
     zip_safe=False,
 )
